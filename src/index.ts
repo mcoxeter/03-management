5874301a115b73b0d4a85e7cd7df6beac41ff958
Michael Coxeter <mcoxeter@gamil.com>
2021-12-20 05:09:49 +0100

    Improved analysis details

--- src/index.ts
+++ src/index.ts
@@ -47,44 +47,21 @@ async function app() {
 
   const roicAnalysis = analyseRoic(periods, roic10);
 
-  // > 1 is a must, > 2 is ideal
-  const current_ratioScores = current_ratio10.map((ratio) => {
-    if (ratio < 1) {
-      return -5;
-    }
-    return ratio >= 2 ? 2 : 1;
-  });
-  const current_ratioScore = current_ratioScores.reduce(
-    (acc, cur) => acc + cur,
-    0
-  );
+  const ratiosAnalysis = analyseRatios(periods, current_ratio10);
 
-  // With debt to equity a lower number is better, <.5 is ideal
-  const debt_to_equityScores = debt_to_equity10.map((x) => (x < 0.5 ? 1 : -10));
-  const debt_to_equityScore = debt_to_equityScores.reduce(
-    (acc, cur) => acc + cur,
-    0
-  );
+  const debtToEquityAnalysis = analyseDebtToEquity(periods, debt_to_equity10);
 
   let management = {
-    type: 'management',
+    type: '03-management',
+    symbol,
+    references: [],
+    date: nowDateStr,
     question1: 'Does the CEO have high levels of stock ownership',
     roicAnalysis,
-    current_ratioScore: {
-      what: 'How is the debt in the companies handled. Current Ratio is the current assets with its total liabilities.',
-      goals: '> 1 is a must, > 2 is ideal',
-      current_ratio: current_ratio10,
-      current_ratioScores,
-      total: current_ratioScore
-    },
-    debt_to_equityScore: {
-      what: 'How leveraged the company is (e.g. Risk)',
-      goals: '.5 or less is ideal',
-      debt_to_equity: debt_to_equity10,
-      debt_to_equityScores,
-      total: debt_to_equityScore
-    },
-    score: roicAnalysis.score + current_ratioScore + debt_to_equityScore
+    ratiosAnalysis,
+    debtToEquityAnalysis,
+    score:
+      roicAnalysis.score + ratiosAnalysis.score + debtToEquityAnalysis.score
   };
 
   console.log('Writing ', `${path}management/${nowDateStr}.json`);
@@ -161,6 +138,131 @@ function analyseRoic(periods: number[], roic: number[]): IROICAnalysis {
     roicAverageScore,
     roicScore,
     score: roicScore + roicAverageScore
+  };
+}
+interface IRatiosAnalysis extends IAnalysis {
+  periods: number[];
+  current_ratios: number[];
+
+  current_ratiosAverage: number;
+  current_ratiosAverageScore: number;
+  current_ratiosScore: number;
+}
+function analyseRatios(
+  periods: number[],
+  current_ratios: number[]
+): IRatiosAnalysis {
+  let greenFlags: string[] = [];
+  let redFlags: string[] = [];
+  let debtTooHighCount = 0;
+
+  const scoreRatio = (ratio: number): number => {
+    if (ratio < 1) {
+      debtTooHighCount++;
+      return -3;
+    }
+    return ratio >= 2 ? 2 : 1;
+  };
+
+  // > 1 is a must, > 2 is ideal
+  const current_ratiosScores = current_ratios.map(scoreRatio);
+  const current_ratiosScore = current_ratiosScores.reduce(
+    (acc, cur) => acc + cur,
+    0
+  );
+
+  const current_ratiosAverage =
+    current_ratios.reduce((acc, cur) => acc + cur, 0) / 10;
+
+  const current_ratiosAverageScore = scoreRatio(current_ratiosAverage);
+
+  if (current_ratiosScore === 20) {
+    greenFlags.push(
+      'Assests are consistanty over 200% liabilities for the last ten years. Debt is managed wonderfully.'
+    );
+  }
+
+  if (debtTooHighCount > 0) {
+    redFlags.push(
+      `Debt compared to assests was too high on ${debtTooHighCount} occasions over the last 10 years.`
+    );
+  }
+
+  return {
+    description:
+      'How is the debt in the companies handled. Current Ratio is the current assets with its total liabilities. > 100% is a must, > 200% is ideal',
+    greenFlags,
+    redFlags,
+    reference: [],
+    periods,
+    current_ratios,
+    current_ratiosAverage,
+    current_ratiosAverageScore,
+    current_ratiosScore,
+    score: current_ratiosScore + current_ratiosAverageScore
+  };
+}
+
+interface IDebtToEquityAnalysis extends IAnalysis {
+  periods: number[];
+  debt_to_equity: number[];
+
+  debt_to_equityAverage: number;
+  debt_to_equityAverageScore: number;
+  debt_to_equityScore: number;
+}
+
+function analyseDebtToEquity(
+  periods: number[],
+  debt_to_equity: number[]
+): IDebtToEquityAnalysis {
+  let greenFlags: string[] = [];
+  let redFlags: string[] = [];
+  let debtToEquityTooHighCount = 0;
+
+  const scoreDebtToEquity = (x: number): number => {
+    if (x < 0.5) {
+      return 1;
+    }
+    debtToEquityTooHighCount++;
+    return -3;
+  };
+
+  const debt_to_equityScores = debt_to_equity.map(scoreDebtToEquity);
+  const debt_to_equityScore = debt_to_equityScores.reduce(
+    (acc, cur) => acc + cur,
+    0
+  );
+
+  const debt_to_equityAverage =
+    debt_to_equity.reduce((acc, cur) => acc + cur, 0) / 10;
+
+  const debt_to_equityAverageScore = scoreDebtToEquity(debt_to_equityAverage);
+
+  if (debt_to_equityScore === 10) {
+    greenFlags.push(
+      'Debt has been consistantly mananged wonderfully over the last ten years.'
+    );
+  }
+
+  if (debtToEquityTooHighCount > 0) {
+    redFlags.push(
+      `Debt compared to equity was too high on ${debtToEquityTooHighCount} occasions over the last 10 years.`
+    );
+  }
+
+  return {
+    description:
+      'How leveraged the company is (e.g. Risk). Debt to equity of .5 or less is ideal',
+    greenFlags,
+    redFlags,
+    reference: [],
+    periods,
+    debt_to_equity,
+    debt_to_equityAverage,
+    debt_to_equityAverageScore,
+    debt_to_equityScore,
+    score: debt_to_equityScore + debt_to_equityAverageScore
   };
 }
 
